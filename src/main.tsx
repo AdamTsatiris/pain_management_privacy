3ba8f4b1548112b4caf2398ee71a703172d770e1
AdamTsatiris <35327659+AdamTsatiris@users.noreply.github.com>
2025-06-05 18:21:47 +0300

    Fix blank preview

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,19 @@ import App from './App';
 import './index.css';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { PainDataProvider } from './contexts/PainDataContext';
+import { AccessibilityProvider } from './contexts/AccessibilityContext';
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+createRoot(rootElement).render(
   <StrictMode>
     <ThemeProvider>
-      <PainDataProvider>
-        <App />
-      </PainDataProvider>
+      <AccessibilityProvider>
+        <PainDataProvider>
+          <App />
+        </PainDataProvider>
+      </AccessibilityProvider>
     </ThemeProvider>
   </StrictMode>
 );
